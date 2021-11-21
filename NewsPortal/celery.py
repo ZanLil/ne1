7bec5e450e2446926100b24dc12a23645e3e79ab
aerastov <a.erastov@gmail.com>
2021-11-21 22:32:12 +0300

    update

--- NewsPortal/celery.py
+++ NewsPortal/celery.py
@@ -6,12 +6,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPortal.settings')
 
 app = Celery('NewsPortal')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
+app.conf.timezone = 'Europe/Moscow'
 app.autodiscover_tasks()
 app.conf.beat_schedule = {
-    'send_mail_every_monday_8am': {
+    'send_mail_monday_8am': {
         'task': 'main_app.tasks.send_mail_monday_8am',
-        'schedule': crontab(hour=8, minute=0, day_of_week=1),
+        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
     },
 }
+
 
